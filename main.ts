444404ad9b23f4bd1f4eb3a11a2a5ce8560ada37
Sergio Acosta <sergio.acosta@bitboxonline.com>
2025-04-01 14:42:36 +0100

    refactor: restructure project by removing main.ts and updating index.ts exports

--- main.ts
+++
@@ -1,6 +0,0 @@
-import { log } from './log';
-
-log.info('User created');
-log.warn('Low disk space');
-log.error('Failed to connect', { service: 'Postgres', retry: true });
-log.debug('Incoming request', { headers: { auth: '*****' } });
